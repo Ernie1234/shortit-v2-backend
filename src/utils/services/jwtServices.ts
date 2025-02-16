4fe94dcec5593810fa6d689c5bd55633805ea30e
Ernie1234 <anumahjoshuaeneye@gmail.com>
2025-02-17 00:22:30 +0100

    Add JWT services and update dependencies for bcryptjs and jsonwebtoken

---
+++ src/utils/services/jwtServices.ts
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken';
+import logger from '@src/logs/logger';
+
+import envConfig from '../../config/envConfig';"
+
+const {JWT_SECRET} = envConfig
+
+interface UserPayload {
+    userId: string;
+    role: string;
+}
